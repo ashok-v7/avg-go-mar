7241ca0313565c8193e8b82c89b8e45a3f263d8d
tknoptn <vashok.tkn@gmail.com>
2023-03-16 16:20:45 +0530

    rsa-implemnt

--- main.go
+++ main.go
@@ -1,35 +1,67 @@
 package main
 
 import (
-    "fmt"
-    "strings"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"os"
+	"time"
 )
 
-// function to take list of prefixes as slice , and input string 
-
-// I need to iterate over the list prefixes using for loop to check if the input string starts with the prefix 
-
-//iterates over the prefixes and checks if the input string starts with the prefix   ---
-// and if the length of the prefix is greater than the length of the current longest prefix found. 
-//If both conditions are true, it updates the longestPrefix variable to be the current prefix.
-
-func findLongestPrefix(prefixes []string, input string) string {
-    longestPrefix := ""
-    for _, prefix := range prefixes {
- 
-		 
-        if strings.HasPrefix(input, prefix) && len(prefix) > len(longestPrefix) {
-            longestPrefix = prefix
-        }
-    }
-    return longestPrefix
-}
-
 func main() {
+	// Generate a 2048-bit RSA key pair
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
 
-	// pass the array of data 
-    prefixes := []string{"tamil","tamilnadu", "nadu",  "allindiaradio", "india", "radio"}
-	input := "allindiaradio  state largest beach in asia"
-    longestPrefix := findLongestPrefix(prefixes, input)
-    fmt.Println(longestPrefix)
+	// Create a self-signed certificate
+	serialNumber, _ := rand.Int(rand.Reader, big.NewInt(999999))
+	subject := pkix.Name{
+		CommonName:   "example.com",
+		Organization: []string{"Example Inc."},
+		Country:      []string{"US"},
+	}
+	template := x509.Certificate{
+		SerialNumber:          serialNumber,
+		Subject:               subject,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1, 0, 0),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &privKey.PublicKey, privKey)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write the private key and certificate to files
+	privKeyFile, err := os.Create("privkey.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer privKeyFile.Close()
+	err = pem.Encode(privKeyFile, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privKey),
+	})
+	if err != nil {
+		panic(err)
+	}
+	certFile, err := os.Create("cert.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer certFile.Close()
+	err = pem.Encode(certFile, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: derBytes,
+	})
+	if err != nil {
+		panic(err)
+	}
 }
